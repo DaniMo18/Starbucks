efed5eb374ef3941758da84a3b5c9e1679b5d13d
Juan Reyes <121842686+LouLuoLou@users.noreply.github.com>
2025-11-11 12:35:20 -0700

    Update Coffee.java

--- Coffee.java
+++ Coffee.java
@@ -1,3 +1,6 @@
+/**
+* This is the class COffee extending Drink
+*/
 public class Coffee extends Drink {
     public Coffee(String name, String size, double price) {
         super(name, size, price);
